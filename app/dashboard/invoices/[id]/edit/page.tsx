d73c5bf3ae00027b66f331a1dabeba6a66c64e3f
André Miani <andremiani@gmail.com>
2024-07-05 22:07:31 -0300

    feat: added authentication

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,24 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
+import { Metadata } from 'next';
 
-        if (!invoice) {
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
+export default async function Page({ params }: { params: { id: string; }; }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
     notFound();
   }
-  
+
   return (
     <main>
       <Breadcrumbs
@@ -26,7 +31,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      <Form invoice={invoice!} customers={customers} />
     </main>
   );
 }
